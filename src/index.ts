f704274d4bb7cf529e4105c2e30080786093b1d7
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-09-02 09:15:42 +0300

    Parsing form data

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes'
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+// urlencoded - will parse the form response
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,30 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hi there');
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form>
+        <div>
+            <label>Email</label>
+            <input name="email" />
+        </div>    
+        <div>
+            <label>Password</label>
+            <input name="password" type="password" />
+        </div>    
+        <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  // to parse the submitted form content we use 'body-parser'
+  // now ew'll have access to 'req.body' which will contain the form content
+  const { email, password } = req.body;
+
+  // TEST
+  res.send(email+password)
 });
 
 export { router };
