cda8430ec9aafb1766c5aa724be386b3a3f847ed
Florin Ionescu <florin.ionescu@everymatrix.com>
2020-09-02 09:34:01 +0300

    Dealing with poor type definition files

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -5,12 +5,16 @@ var express_1 = require("express");
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
-    res.send("\n    <form>\n        <div>\n            <label>Email</label>\n            <input name=\"email\" />\n        </div>    \n        <div>\n            <label>Password</label>\n            <input name=\"password\" type=\"password\" />\n        </div>    \n        <button>Submit</button>\n    </form>\n  ");
+    res.send("\n    <form method=\"POST\">\n        <div>\n            <label>Email</label>\n            <input name=\"email\" />\n        </div>    \n        <div>\n            <label>Password</label>\n            <input name=\"password\" type=\"password\" />\n        </div>    \n        <button>Submit</button>\n    </form>\n  ");
 });
 router.post('/login', function (req, res) {
     // to parse the submitted form content we use 'body-parser'
     // now ew'll have access to 'req.body' which will contain the form content
     var _a = req.body, email = _a.email, password = _a.password;
-    // TEST
-    res.send(email + password);
+    if (email) {
+        res.send(email.toUpperCase());
+    }
+    else {
+        res.send('You must provide an email');
+    }
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response } from 'express';
 
+// for dealing with poor type definition files
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
   res.send(`
-    <form>
+    <form method="POST">
         <div>
             <label>Email</label>
             <input name="email" />
@@ -18,13 +23,16 @@ router.get('/login', (req: Request, res: Response) => {
   `);
 });
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   // to parse the submitted form content we use 'body-parser'
   // now ew'll have access to 'req.body' which will contain the form content
   const { email, password } = req.body;
 
-  // TEST
-  res.send(email+password)
+  if (email) {
+    res.send(email.toUpperCase());
+  } else {
+    res.send('You must provide an email');
+  }
 });
 
 export { router };
